b3298a920da361fb9158a54b1adb2d9f162e43e0
Gavin Whyte <whyteg@Gavins-MBP.home>
2014-12-17 11:46:42 +1100

    initial commit

---
+++ manifests/nodes.pp
@@ -0,0 +1,15 @@
+'host1' {
+  include java 
+  class { 'zookeeper':
+        myid  => '1',
+        package_url => 'http://apache.mirror.serversaustralia.com.au/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz'
+    }
+
+    class { 'kafka':
+            broker_id  => '0',
+            hostname => $::ipaddress_eth1, # $::ipaddress is picked by default
+            zookeeper_connect => 'localhost:2181',
+            package_url => 'http://mirror.symnds.com/software/Apache/kafka/0.8.1.1/kafka_2.10-0.8.1.1.tgz'
+          }
+}
+
---
+++ modules/kafka/manifests/init.pp
@@ -0,0 +1,20 @@
+class kafka (
+  $broker_id,
+  $hostname = $kafka::params::hostname,
+  $zookeeper_connect = $kafka::params::zookeeper_connect,
+  $package_dir = $kafka::params::package_dir,
+  $package_url = $kafka::params::package_url,
+  $install_dir = $kafka::params::install_dir,
+  $statsd_host = $kafka::params::statsd_host,
+  $statsd_port = $kafka::params::statsd_port ) inherits kafka::params {
+
+  require java
+
+  anchor { 'kafka::begin': } ->
+  class { '::kafka::package': } ->
+  class { '::kafka::install': } ->
+  class { '::kafka::config': } ->
+  class { '::kafka::service': } ->
+  anchor { 'kafka::end': }
+
+}
