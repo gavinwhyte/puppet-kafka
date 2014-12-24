b573c61bbb7c4406a9e46bc1127bd591d289e431
Gavin Whyte <whyteg@Gavins-MacBook-Pro.local>
2014-12-24 12:23:56 +1100

    test

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,12 +1,16 @@
-node 'ssd1sanfran2gig' {
-   
-   include java 
-   include kafka
+node default {
 }
+
 
 node 'ssd2sanfran2gig' {
    
    include java 
    include zookeeper
   
+}
+
+node 'ssd1sanfran2gig' {
+   
+   include java 
+   include kafka
 }
