4bd7f4f61fea3662d69e53196014ef11592df6e1
Gavin Whyte <whyteg@Gavins-MacBook-Pro.local>
2015-01-06 16:26:03 +1100

    fix permissions

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -11,6 +11,29 @@ node 's025wrtl2299.s4.chp.cba' {
     }
 }
 
+
+node 's025wrtl6208.s4.chp.cba' {
+   
+   include java 
+
+   class { 'kafka':
+      broker_id => '0',
+      hostname => $::ipaddress,
+      zookeeper_connect => '10.39.3.96:2181'
+    }
+}
+
+node 's025wrtl1167.s4.chp.cba' {
+   
+   include java 
+
+   class { 'kafka':
+      broker_id => '0',
+      hostname => $::ipaddress,
+      zookeeper_connect => '10.39.3.96:2181'
+    }
+}
+
 node 's025wrtl1802.s4.chp.cba' {
    
    include java 
